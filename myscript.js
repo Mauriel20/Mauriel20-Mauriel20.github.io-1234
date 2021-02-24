3e4e45472dd7f54acc660423c7b5bfd8784c70e3
Mauriel <josueazofeifajosue@gmail.com>
2021-02-24 16:06:56 +0000

    My-work

--- myscript.js
+++ myscript.js
@@ -2,16 +2,15 @@
             let element = document.getElementById("texto1");
             element.classList.toggle("dark-mode");
 
-
-
-            let element2 = document.querySelectorAll('.card');
-            element2.classList.toggle("dark-mode2");
-
+            let element2 = document.querySelectorAll('div.card');
+            for (let index = 0; index < element2.length; index++) {
+                element2[index]["style"].color = "black";
+            }
+           
             const switchButton = document.getElementById('switch');
             switchButton.classList.toggle('active');//toggle the HTML button with the id='switch' with the class 'active'
 
         }
-
 
            function outputbox01(vol) {
             document.querySelector('#fontsize').value = vol;
