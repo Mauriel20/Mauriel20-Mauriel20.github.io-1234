607f856ba90a63c124f5a3f4b6c59c95696c360a
Mauriel <josueazofeifajosue@gmail.com>
2021-02-25 16:10:49 +0000

    My-work

--- myscript.js
+++ myscript.js
@@ -29,6 +29,29 @@
             $('#texto1').css("font-size", $(this).val() + "px");
         });
 
+$(document).keydown(function(event) {
+if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+        event.preventDefault();
+     }
+    // 107 Num Key  +
+    // 109 Num Key  -
+    // 173 Min Key  hyphen/underscor Hey
+    // 61 Plus key  +/= key
+});
 
+$(window).bind('mousewheel DOMMouseScroll', function (event) {
+       if (event.ctrlKey == true) {
+       event.preventDefault();
+       }
+});
+
+$(document).ready(function(){
+$(document).keydown(function(event) {
+    if (event.ctrlKey==true && (event.which == '107' || event.which == '109')) {
+        alert('disabling zooming ! ');
+        event.preventDefault();
+     }
+});
+})
         
      
